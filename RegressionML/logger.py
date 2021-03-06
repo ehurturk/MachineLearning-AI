87e84e6314a6f8c95155e37429b3d1c6c71f36c6
Emir Hurturk <emirhurturk444@gmail.com>
2021-03-06 16:18:03 +0300

    A new logging system

--- RegressionML/logger.py
+++ RegressionML/logger.py
@@ -6,21 +6,33 @@ class Logger:
     def __init__(self):
         self.__dir = os.path.dirname(__file__)
         self.__filename = os.path.join(self.__dir, "log.json")
-        self.logm = {}
-        self.date = datetime.datetime.now().strftime("%m/%d/%Y")
-        self.logm[self.date] = []
+        self.data = None
+        self.date = datetime.datetime.now().strftime("%m/%d/%YT%H/%M/%S")
+        self.message = [] # change later
+
+
 
     def log(self, total_iter):
-        self.logm[self.date].append({"Total Iterations": total_iter})
-        with open(self.__filename, "a") as out:
-            json.dump(self.logm, out)
+        self.message.append({"Total Iterations": total_iter})
+        self.data.append(self.message)
+
+        with open(self.__filename, "w") as out:
+            json.dump(self.data, out)
 
     def msg(self, params, iter_n, cost):
-        self.logm[self.date].append({
+        with open(self.__filename, "r") as jsondata:
+            try:
+                self.data = json.load(jsondata)
+            except json.JSONDecodeError:
+                self.data = []
+
+        self.message.append({
                 "Iteration": iter_n,
                 "Cost": cost,
                 "Parameters": list(params),
         })
+
+
 
 
 
